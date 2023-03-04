4ab9b365cfdfd79c27505dba3f7432b8b949d2fb
sahill-1 <sv260397@gmail.com>
2023-03-04 15:31:22 +0530

    data added using map

--- frontend/src/Components/FeatureProduct.jsx
+++ frontend/src/Components/FeatureProduct.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-export default function FeatureProduct() {
+export default function FeatureProduct(props) {
     return (
             <div className='card' style={{ border: "1px solid black", width:"80%", margin:"auto"}}>
-                <img style={{ width: "100%", borderRadius: "20px" }} className='product--image' src='https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80' alt='' />
-                <h2>Product Name</h2>
-                <p>add the description to the products</p>
+                <img style={{ width: "100%", borderRadius: "20px" }} className='product--image' src={props.url} alt='' />
+                <h2>{props.name}</h2>
+                <p style={{color:"gray"}}>Sold by: xyz</p>
+                <p>{props.desc}</p>
+                <p>
+                    <button>Get Enquiry</button>
+                </p>
             </div>
     )
 }
--- frontend/src/Components/FeaturedProducts.js
+++ frontend/src/Components/FeaturedProducts.js
@@ -8,15 +8,15 @@ const FeaturedProducts = () => {
     const responsive = {
         superLargeDesktop: {
             // the naming can be any, depends on you.
-            breakpoint: { max: 4000, min: 3000 },
-            items: 5
+            breakpoint: { max: 4000, min: 1024 },
+            items: 4
         },
         desktop: {
-            breakpoint: { max: 3000, min: 1024 },
+            breakpoint: { max: 1024, min: 800 },
             items: 3
         },
         tablet: {
-            breakpoint: { max: 1024, min: 464 },
+            breakpoint: { max: 800, min: 464 },
             items: 2
         },
         mobile: {
@@ -24,19 +24,72 @@ const FeaturedProducts = () => {
             items: 1
         }
     };
+
+    const productData = [
+        {
+            id: 1,
+            imageurl: "https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80",
+            name: "Product One",
+            sold: "Sold by - XYZ",
+            description: "Add the description about the product..."
+        },
+        {
+            id: 2,
+            imageurl: "https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80",
+            name: "Product Two",
+            sold: "Sold by - XYZ",
+            description: "Add the description about the product..."
+        },
+        {
+            id: 3,
+            imageurl: "https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80",
+            name: "Product Three",
+            sold: "Sold by - XYZ",
+            description: "Add the description about the product..."
+        },
+        {
+            id: 4,
+            imageurl: "https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80",
+            name: "Product Four",
+            sold: "Sold by - XYZ",
+            description: "Add the description about the product..."
+        },
+        {
+            id: 5,
+            imageurl: "https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80",
+            name: "Product FIve",
+            sold: "Sold by - XYZ",
+            description: "Add the description about the product..."
+        },
+        {
+            id: 6,
+            imageurl: "https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80",
+            name: "Product Six",
+            sold: "Sold by - XYZ",
+            description: "Add the description about the product..."
+        },
+        {
+            id: 7,
+            imageurl: "https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80",
+            name: "Product Seven",
+            sold: "Sold by - XYZ",
+            description: "Add the description about the product..."
+        },
+        {
+            id: 8,
+            imageurl: "https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80",
+            name: "Product Eight",
+            sold: "Sold by - XYZ",
+            description: "Add the description about the product..."
+        }
+    ]
+
+    const product = productData.map((item) => <FeatureProduct name={item.name} url={item.imageurl} desc={item.description} /> )
     return (
         <div>
             <h1 style={{ textDecoration: "underline" }}>Featured Products</h1>
             <Carousel responsive={responsive}>
-                <FeatureProduct/>
-                <FeatureProduct/>
-                <FeatureProduct/>
-                <FeatureProduct/>
-                <FeatureProduct/>
-                <FeatureProduct/>
-                <FeatureProduct/>
-                <FeatureProduct/>
-
+                {product}
             </Carousel>;
         </div>
     )
