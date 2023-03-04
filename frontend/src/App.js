0eecebb205497723fe0509fbc8f71482bc9fc276
sahill-1 <sv260397@gmail.com>
2023-03-04 10:03:51 +0530

    product slider

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ProductSlider from "../src/Components/ProductSlider";
+function App() {
+  return (
+    <div className="App">
+      <ProductSlider/>
+      <ProductSlider/>
+      <ProductSlider/>
+    </div>
+  );
+}
+
+export default App;
