f7004a1296697926af08cfffe94bb4dc92052efb
sahill-1 <sv260397@gmail.com>
2023-03-14 10:42:41 +0530

    testimonial done

--- frontend/src/Components/Testimonial.js
+++ frontend/src/Components/Testimonial.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import "./Testimonial.css";
-import "./data";
+// import {data} from "../data";
+import data from "../data"
 
 const Testimonial = () => {
+
   const [people] = useState(data);
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
     const lastIndex = people.length - 1;
-    if(index < 0){
+    if (index < 0) {
       setIndex(lastIndex);
     }
-    if(index > lastIndex){
+    if (index > lastIndex) {
       setIndex(0);
     }
   }, [index, people]);
@@ -30,8 +32,32 @@ const Testimonial = () => {
 
   return (
     <>
-      <h2>Testimonial</h2>
+      <p style={{ color: "#444444", fontWeight: "500", fontSize: "34px", paddingTop:"25px" }}>What Our Client Say About Us</p>
+      <section className='section my-2'>
+        <div className='section-center'>
+          {people.map((item, indexPeople) => {
+            const { id, name, image, quote } = item;
+            let position = "nextSlide";
+            if (indexPeople === index) {
+              position = "activeSlide"
+            }
+            if (indexPeople === index - 1 || (index === 0 && indexPeople === people.length - 1)) {
+              position = "lastSlide"
+            }
+            return (
+              <article className={position} key={id}>
+                <img src={image} alt={name} className="person-img" />
+                <h4>{name}</h4>
+                <p className='text'>{quote}</p>
+              </article>
+            )
+          })}
+          <button className='prev' onClick={() => setIndex(index-1)}><i className='fas fa-arrow-left'/></button>
+          <button className='next' onClick={() => setIndex(index+1)}><i className='fas fa-arrow-right'/></button>
+        </div>
+      </section>
     </>
+
   )
 }
 
--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,4 +1,4 @@
-let people = [
+const people = [
     {
         id: 1,
         name: "Person One",
@@ -17,4 +17,6 @@ let people = [
         image: "https://www.shutterstock.com/image-photo/surreal-image-african-elephant-wearing-260nw-1365289022.jpg",
         quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
     }
-]
+];
+
+export default people;
