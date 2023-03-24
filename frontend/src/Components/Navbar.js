1a696435ff9ae190188c76517cf6ea949de333ba
sahill-1 <sv260397@gmail.com>
2023-03-24 12:05:50 +0530

    navbar done

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -6,7 +6,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import logo from "../images/bwt-dark.png";
 import { BsFillMicFill } from 'react-icons/bs';
-import {FiHeadphones} from "react-icons/fi";
+import { FiHeadphones } from "react-icons/fi";
 import ProductMenu from '../Components/ProductMenu';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
@@ -17,7 +17,7 @@ const NavbarHeader = () => {
         <>
             <Navbar bg="light" expand="lg">
                 <Container fluid>
-                    <Navbar.Brand href="/"><img src={logo} alt="error" style={{ width: "300px", height: '60px', marginLeft: "40px" }} /></Navbar.Brand>
+                    <Navbar.Brand href="/"><img src={logo} alt="error" style={{ width: "250px", height: '60px', marginLeft: "20px" }} /></Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                         {/* <Nav
@@ -57,22 +57,22 @@ const NavbarHeader = () => {
                         <div className="d-flex buyer-seller" style={{ paddingRight: "130px" }}>
                             <NavDropdown title="Buyer" id="navbarScrollingDropdown" style={{ padding: "0px 20px 0px 0px" }}>
                                 <NavDropdown.Item href="#action3">Post Buy Requirements</NavDropdown.Item>
-                                {/* <NavDropdown.Divider /> */}
+                                <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action4">Search Suppliers</NavDropdown.Item>
-                                {/* <NavDropdown.Divider /> */}
+                                <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action5">Request A Callback</NavDropdown.Item>
                             </NavDropdown>
                             <NavDropdown title="Seller" id="navbarScrollingDropdown" >
                                 <NavDropdown.Item href="#action3">Search Buy Leads</NavDropdown.Item>
-
+                                <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action4">Get Freight Quotes</NavDropdown.Item>
-
+                                <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action5">Logistics</NavDropdown.Item>
-
+                                <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action5">Export Bill Discounting</NavDropdown.Item>
-
+                                <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action5">WTC Certificate</NavDropdown.Item>
-
+                                <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action5">Export Data</NavDropdown.Item>
                             </NavDropdown>
                         </div>
@@ -81,20 +81,27 @@ const NavbarHeader = () => {
                 </Container>
             </Navbar>
 
-            <Navbar className='d-flex justify-content-between' bg="light" expand="lg" style={{ border: "1px solid blue", marginTop: "-20px", padding: "0px 80px 0px 20px" }}>
-                <div>
-                    <ProductMenu />
-                </div>
-                <div>
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
-                    >
-                        <Nav.Link href="/" className="right-section"><FiHeadphones/>  Help Center</Nav.Link>
-                        <Nav.Link href="/" className="right-section">Log In</Nav.Link>
-                        <Nav.Link href="/" className="right-section">Register Free</Nav.Link>
-                    </Nav>
-                </div>
+            {/* SECOND NAVBAR HERE */}
+
+            <Navbar className=' navbar-bottom' bg="light" expand="lg" style={{ marginTop: "-20px", padding: "0px 30px 0px 20px" }}>
+                <Container fluid>
+                    <Navbar.Collapse className="d-flex justify-content-between Navbar-Collapse">
+                        <div className="product-Menu">
+                            <ProductMenu />
+                        </div>
+                        <div className="Help-Center">
+                            <Nav
+                                className="me-auto my-2 my-lg-0 "
+                                style={{ maxHeight: '100px'}}
+                            >
+                                <Nav.Link href="/" className="mx-2 right-section"><FiHeadphones />  Help Center</Nav.Link>
+                                <Nav.Link href="/" className="mx-2 right-section ">Log In</Nav.Link>
+                                <Nav.Link href="/" className="mx-2 right-section">Register Free</Nav.Link>
+                            </Nav>
+                        </div>
+                    </Navbar.Collapse>
+                </Container>
+
             </Navbar>
         </>
     );
