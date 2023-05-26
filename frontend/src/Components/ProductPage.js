0b3f1e0fdb83b4243cfa8db04c583dc5081e9033
sahill-1 <sv260397@gmail.com>
2023-05-26 10:39:46 +0530

    Product page

--- frontend/src/Components/ProductPage.js
+++ frontend/src/Components/ProductPage.js
@@ -21,9 +21,9 @@ const ProductPage = () => {
       </div>
 
       <div className="product-details">
-        <h1 className="product-name">XYZ Smartwatch</h1>
+        <h1 className="product-name">Product Title</h1>
         <h2 className="product-price">Rs.1999/-</h2>
-        <p className="product-availability">In Stock</p>
+        {/* <p className="product-availability">In Stock</p> */}
         <div className="product-rating">
           <span className="rating-stars">★★★★☆</span>
           <span className="rating-score">(4.5/5)</span>
@@ -34,8 +34,8 @@ const ProductPage = () => {
 
         <div className="key-features">
           <h3>Key Features:</h3>
-          <ul>
-            <li>Sleek and stylish design</li>
+          <ul style={{border:"1px solid transparent", width:"50%", margin:"auto"}}>
+            <li >Sleek and stylish design</li>
             <li>Vibrant OLED display for clear visibility</li>
             <li>Heart rate monitor to track your fitness levels</li>
             <li>Built-in GPS for accurate location tracking</li>
@@ -44,20 +44,26 @@ const ProductPage = () => {
             <li>Water-resistant for all-day wear</li>
           </ul>
         </div>
-        <div className="product-highlights">
+
+        <div className="actions">
+          <button className="add-to-cart">Enquiry Now</button>
+        </div>
+
+      </div>
+      <div className="my-4 product-highlights" style={{width:"90%",margin:"auto", textAlign:"left"}}>
           <h3>Product Highlights:</h3>
           <ul>
             <li>Stay connected: Receive notifications for calls, messages, and social media alerts directly on your wrist.</li>
             <li>Track your fitness: Monitor your steps, distance, calories burned, and sleep patterns to achieve your health goals.</li>
             <li>GPS tracking: Stay on track with accurate location data during outdoor activities like running or hiking.</li>
-            <li>Long battery life: Enjoy up to 5 days of usage on a single charge, so you can focus on your activities without interruption.</li>
+            {/* <li>Long battery life: Enjoy up to 5 days of usage on a single charge, so you can focus on your activities without interruption.</li> */}
             <li>Stylish and durable: The XYZ Smartwatch is designed to complement your style and withstand daily wear and tear.</li>
             <li>Easy synchronization: Pair the watch with your smartphone to access additional features and customization options.</li>
           </ul>
         </div>
 
-        <div className="customer-reviews">
-          <h3>Customer Reviews:</h3>
+        <div className="customer-reviews" style={{width:"90%",margin:"auto", textAlign:"left"}}>
+          <h3 style={{fontSize:"24px"}}>Customer Reviews:</h3>
           <div className="review">
             <p>"I love my XYZ Smartwatch! It's sleek, comfortable to wear, and keeps me motivated to reach my fitness goals." - Sarah</p>
           </div>
@@ -68,13 +74,6 @@ const ProductPage = () => {
             <p>"The XYZ Smartwatch looks great on my wrist. It's lightweight, and the display is crystal clear. Highly recommended!" - Emily</p>
           </div>
         </div>
-
-        <div className="actions">
-          <button className="add-to-cart">Add to Cart</button>
-          <button className="buy-now">Buy Now</button>
-        </div>
-
-      </div>
     </div>
     <Footer/>
     </>
