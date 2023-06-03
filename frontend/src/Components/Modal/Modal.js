151c0a7da41bb2b50031f32ef4efb761151ba7c7
sahill-1 <sv260397@gmail.com>
2023-06-03 10:38:40 +0530

    popmodal animation

--- frontend/src/Components/Modal/Modal.js
+++ frontend/src/Components/Modal/Modal.js
@@ -17,11 +17,9 @@ const Modal = () => {
       {isOpen && (
         <div className="modal-overlay">
           <div className="modal-content">
-            <span className="close" onClick={toggleModal}>
+            <span className="close " onClick={toggleModal}>
               &times;
             </span>
-            {/* <h2>Modal Title</h2>
-            <p>This is a sample modal.</p> */}
             <div className="modal-form-style">
               <FloatingLabel style={{ outline: "none !important" }} controlId="floatingText" label="First & Last Name" className="mb-2">
                 <Form.Control type="text" placeholder="Full Name" />
@@ -36,22 +34,18 @@ const Modal = () => {
               <FloatingLabel controlId="floatingText" label="Type your query" className="mb-2">
                 <Form.Control type="text" placeholder="Text" />
               </FloatingLabel>
-              <Dropdown as={ButtonGroup} style={{ position: "relative", right: "40%" }}>
+              <Dropdown as={ButtonGroup} style={{ position: "relative", right: "30%" }}>
                 <Button variant="success">I am</Button>
-
                 <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
-
                 <Dropdown.Menu style={{ width: "auto", padding: "6px" }}>
                   <Dropdown.Item style={{ padding: "4px" }} href="#/action-1">Buyer</Dropdown.Item>
                   <Dropdown.Item style={{ padding: "4px" }} href="#/action-2">Seller</Dropdown.Item>
-
                 </Dropdown.Menu>
               </Dropdown>
-              {/* <Button className="my-2" variant="primary" size="md">
-              Submit
-            </Button> */}
+
+              <Button style={{position:"relative", left:"30%", backgroundColor:"red", borderStyle:"none"}} className="my-2" variant="primary" size="md">Submit</Button>
             </div>
-            
+
           </div>
         </div>
       )}
@@ -59,4 +53,4 @@ const Modal = () => {
   );
 };
 
-export default Modal;
+export default Modal;     
