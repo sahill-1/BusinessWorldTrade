b519168810171a37dedadbafb1986cea255b4bea
sahill-1 <sv260397@gmail.com>
2023-06-08 10:19:34 +0530

    about us responsive

--- frontend/src/Components/FooterPages/AboutUS.js
+++ frontend/src/Components/FooterPages/AboutUS.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import Navbar from "../Homepage/Navbar";
 import Footer from '../Homepage/Footer';
+import { BsFillChatTextFill } from 'react-icons/bs';
+import { TbCategory } from 'react-icons/tb';
+import { FaUsers } from 'react-icons/fa';
+import { BiSupport } from 'react-icons/bi';
 
 const AboutUS = () => {
     return (
         <div>
             <Navbar />
+            {/* FIRST_DIV */}
             <div className='about-us-first-div my-5' >
                 <div className='about-us-first-div-left'>
-                    <h2 style={{ color: "#21496c" }}>Making Indian MSMEs</h2>
-                    <h2><span style={{ color: "red" }}>Digitally</span><span style={{ color: "#f3c110" }} >Saksham</span></h2>
-                    <p>We are one of <span style={{ color: "#464545" }}>India’s largest and oldest B2B platforms</span> India’s largest and oldest B2B platforms that
-                        connects buyers and suppliers to create a customer-driven value chain for
-                        all businesses, including SMEs, large enterprises, and individuals.</p>
-                    <p>We integrate businesses into global value chains by bringing them together
-                        from across the world, on a single platform to interact and conduct the
-                        business smoothly, securely, and effectively.
+                    <h2 style={{ color: "#2d5e79" }}>Making Indian MSMEs</h2>
+                    <h2><span style={{ color: "red" }}>Digitally </span><span style={{ color: "#2d5e79" }} >Saksham</span></h2>
+                    <p>Welcome to <span style={{ color: "#464545" }}>Business World Trade, a premier B2B platform</span>  connecting buyers and sellers from around the globe. We are dedicated to revolutionizing the way businesses trade and thrive in today's dynamic marketplace.</p>
+                    <p>At Business World Trade, our mission is to provide a robust and seamless platform that facilitates efficient and transparent transactions between buyers and sellers. We believe in empowering businesses of all sizes by creating opportunities for growth, collaboration, and success.
                     </p>
                 </div>
                 <div className='about-us-first-div-right'>
@@ -39,76 +40,102 @@ const AboutUS = () => {
                     </div>
 
                     <div className='first-animation-slider'>
-                        <div className='first-animation-slider-img'>
-                            <img src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="error" />
+                        <div className='first-animation-slider-img-two'>
+                            <img src="https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_1280.jpg" alt="error" />
                         </div>
-                        <div className='first-animation-slider-img'>
-                            <img src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="error" />
+                        <div className='first-animation-slider-img-two'>
+                            <img src="https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_1280.jpg" alt="error" />
                         </div>
-                        <div className='first-animation-slider-img'>
-                            <img src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="error" />
+                        <div className='first-animation-slider-img-two'>
+                            <img src="https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_1280.jpg" alt="error" />
                         </div>
-                        <div className='first-animation-slider-img'>
-                            <img src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="error" />
+                        <div className='first-animation-slider-img-two'>
+                            <img src="https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_1280.jpg" alt="error" />
                         </div>
-                        <div className='first-animation-slider-img'>
-                            <img src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="error" />
+                        <div className='first-animation-slider-img-two'>
+                            <img src="https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_1280.jpg" alt="error" />
                         </div>
+
                     </div>
 
                     <div className='first-animation-slider' >
                         <div className='first-animation-slider-img'>
-                            <img src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="error" />
+                            <img src="https://media.gettyimages.com/id/1297349747/photo/hot-air-balloons-flying-over-the-botan-canyon-in-turkey.jpg?s=612x612&w=gi&k=20&c=Uo_yzYm9UJu6GpKilOLGrCbiSjyMB5DsvZTYpybYxj4=" alt="error" />
                         </div>
                         <div className='first-animation-slider-img'>
-                            <img src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="error" />
+                            <img src="https://media.gettyimages.com/id/1297349747/photo/hot-air-balloons-flying-over-the-botan-canyon-in-turkey.jpg?s=612x612&w=gi&k=20&c=Uo_yzYm9UJu6GpKilOLGrCbiSjyMB5DsvZTYpybYxj4=" alt="error" />
                         </div>
                         <div className='first-animation-slider-img'>
-                            <img src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="error" />
+                            <img src="https://media.gettyimages.com/id/1297349747/photo/hot-air-balloons-flying-over-the-botan-canyon-in-turkey.jpg?s=612x612&w=gi&k=20&c=Uo_yzYm9UJu6GpKilOLGrCbiSjyMB5DsvZTYpybYxj4=" alt="error" />
                         </div>
                         <div className='first-animation-slider-img'>
-                            <img src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="error" />
+                            <img src="https://media.gettyimages.com/id/1297349747/photo/hot-air-balloons-flying-over-the-botan-canyon-in-turkey.jpg?s=612x612&w=gi&k=20&c=Uo_yzYm9UJu6GpKilOLGrCbiSjyMB5DsvZTYpybYxj4=" alt="error" />
                         </div>
                         <div className='first-animation-slider-img'>
-                            <img src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="error" />
+                            <img src="https://media.gettyimages.com/id/1297349747/photo/hot-air-balloons-flying-over-the-botan-canyon-in-turkey.jpg?s=612x612&w=gi&k=20&c=Uo_yzYm9UJu6GpKilOLGrCbiSjyMB5DsvZTYpybYxj4=" alt="error" />
                         </div>
                     </div>
                 </div>
             </div>
 
+            {/* ABOUT_US */}
             <div className='aboutus-second'>
-            <div className='about-us-second-div'>
-                <div className='about-us-second-div-block'>
-                    <div className='about-us-second-div-block-left'>
-                        <h2>OUR MISSION</h2>
-                        <h4>Let’s enable 63 million+ SMEs to go digital</h4>
-                        <p>With technology and innovation as enablers, we endeavour to help our users reinvent businesses to compete and win, with digitization at the core. We also pride ourselves to be the only marketplace offering 360° digital marketing solutions to MSMEs to help them be tech-enabled. <br></br><br></br>With a robust pan- India as well as a global presence in 10+ countries, we also assist global buyers to locate Indian suppliers, manufacturers, and exporters.</p>
+                <div className='about-us-second-div'>
+                    <div className='about-us-second-div-block'>
+                        <div className='about-us-second-div-block-left'>
+                            <h2>OUR MISSION</h2>
+                            <h4>Revolutionizing B2B Trade, Empowering Businesses, and Connecting Global Success</h4>
+                            <p>Business World Trade is to revolutionize global trade by providing a seamless and trusted B2B platform that connects buyers and sellers. We are dedicated to empowering businesses of all sizes, fostering transparency, and creating opportunities for growth and success. Through our innovative solutions, we aim to simplify the trading process, facilitate efficient transactions, and build strong, long-lasting business relationships.</p>
+                        </div>
+                        <div className='about-us-second-div-block-right'>
+                            <img src='https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg' alt='' />
+                        </div>
                     </div>
-                    <div className='about-us-second-div-block-right'>
-                        <img src='https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg' alt='' />
+                    <div className='about-us-second-div-block-two'>
+                        <div className='about-us-second-div-block-right'>
+                            <img src='https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg' alt='' />
+                        </div>
+                        <div className='about-us-second-div-block-left'>
+                            <h2>OUR VISION</h2>
+                            <h4>Redefining Global Trade, Inspiring Growth, and Empowering Businesses for a Thriving Future.</h4>
+                            <p>Business World Trade is to be the leading global B2B platform that empowers businesses to thrive and succeed in the digital age. We envision a future where trade barriers are minimized, connections are effortlessly made, and businesses can access a world of opportunities with ease. We strive to be at the forefront of innovation, continuously evolving our platform to meet the changing needs of the business community. By enabling seamless collaboration and facilitating trusted transactions, we aspire to be the driving force behind the growth and prosperity of businesses worldwide.</p>
+                        </div>
                     </div>
-                </div>
-                <div className='about-us-second-div-block-two'>
-                    <div className='about-us-second-div-block-right'>
-                        <img src='https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg' alt='' />
-                    </div>
-                    <div className='about-us-second-div-block-left'>
-                        <h2>OUR VISION</h2>
-                        <h4>Fostering Sustainable Partnerships for B2B Excellence and Growth</h4>
-                        <p>In harmony with our core values and stake holders, our goal is to be the largest and finest B2B marketplace with bunch of incredible features, unsurpassed services and everlasting support. <br></br><br></br> Acquiring effective digital expertise to help connect traders in developing unique brand persona.</p>
-                    </div>
-                </div>
-                <div className='about-us-second-div-block'>
-                <div className='about-us-second-div-block-left'>
-                        <h2>OUR GOALS</h2>
-                        <h4>Our Goals are Customer Satisfaction and Market Leadership</h4>
-                        <p>With technology and innovation as enablers, we endeavour to help our users reinvent businesses to compete and win, with digitization at the core. We also pride ourselves to be the only marketplace offering 360° digital marketing solutions to MSMEs to help them be tech-enabled. <br></br><br></br>With a robust pan- India as well as a global presence in 10+ countries, we also assist global buyers to locate Indian suppliers, manufacturers, and exporters.</p>
-                    </div>
-                    <div className='about-us-second-div-block-right'>
-                        <img src='https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg' alt='' />
+                    <div className='about-us-second-div-block'>
+                        <div className='about-us-second-div-block-left'>
+                            <h2>OUR GOALS</h2>
+                            <h4>Empowering Businesses, Fostering Collaboration, and Driving Global Success through Innovative Solutions</h4>
+                            <p>Business World Trade is to revolutionize the B2B trading landscape by providing a robust and seamless platform for businesses to connect, grow, and thrive. We aim to foster business growth by expanding their reach and opening new markets. Our platform enables seamless transactions, promoting secure and transparent communication between buyers and sellers. With a focus on trust, transparency, and fair trade practices, we strive to build long-lasting relationships and facilitate collaborations that drive mutual success.</p>
+                        </div>
+                        <div className='about-us-second-div-block-right'>
+                            <img src='https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg' alt='' />
+                        </div>
                     </div>
                 </div>
             </div>
+
+            {/* LAST_DIV */}
+            <div className='about-us-last-div my-5'>    
+                <div className='about-us-last-div-section'>
+                    <TbCategory style={{color:"red", fontSize:"43px"}}/>
+                    <h4 className='about-us-last-div-section-h'>80,000+ </h4>
+                    <p className='about-us-last-div-section-p'>Product Categories</p>
+                </div>
+                <div className='about-us-last-div-section'>
+                    <FaUsers style={{color:"red", fontSize:"45px"}}/>
+                    <h4 className='about-us-last-div-section-h'>1 Million+ </h4>
+                    <p className='about-us-last-div-section-p'>Registered Users</p>
+                </div>
+                <div className='about-us-last-div-section'>
+                    <BsFillChatTextFill style={{color:"red", fontSize:"40px"}}/>
+                    <h4 className='about-us-last-div-section-h'>1000+ Inquiries</h4>
+                    <p className='about-us-last-div-section-p'>Every Day</p>
+                </div>
+                <div className='about-us-last-div-section'>
+                    <BiSupport style={{color:"red", fontSize:"40px"}} />
+                    <h4 className='about-us-last-div-section-h'>24/7</h4>
+                    <p className='about-us-last-div-section-p'>Customer Support</p>
+                </div>
             </div>
             <Footer />
         </div>
