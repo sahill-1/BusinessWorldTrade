02b07cfedf58bcdeae6fc667da012bfb26874be6
AnikitaSinghal <ani.singhal178@gmail.com>
2023-06-14 12:32:40 +0530

    undo changes

--- frontend/src/Components/Homepage/AboutMe.js
+++ frontend/src/Components/Homepage/AboutMe.js
@@ -8,7 +8,7 @@ const AboutMe = () => {
       <img src="http://cdn.shopify.com/s/files/1/0086/0795/7054/articles/Cat_s_Mind_x630.jpg?v=1624444348" alt="error" />
       <div className="paragraph-slider">
 
-        {/* <p>Paragraph 1 It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p> */}
+        <p>Paragraph 1 It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
         <p>Paragraph 2 It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
         <p>Paragraph 3 It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
 
