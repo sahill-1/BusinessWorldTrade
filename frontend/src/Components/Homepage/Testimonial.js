42b35ebed2ac99b25da63b6f111e7b599d3ca39f
sahill-1 <sv260397@gmail.com>
2023-06-19 10:56:40 +0530

    testimonial change

--- frontend/src/Components/Homepage/Testimonial.js
+++ frontend/src/Components/Homepage/Testimonial.js
@@ -14,6 +14,12 @@ const testimonials = [
     feedback: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     image: 'https://1.bp.blogspot.com/-kK7Fxm7U9o0/YN0bSIwSLvI/AAAAAAAACFk/aF4EI7XU_ashruTzTIpifBfNzb4thUivACLcBGAsYHQ/s1280/222.jpg',
   },
+  {
+    id: 3,
+    name: 'Shhani',
+    feedback: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: 'https://1.bp.blogspot.com/-kK7Fxm7U9o0/YN0bSIwSLvI/AAAAAAAACFk/aF4EI7XU_ashruTzTIpifBfNzb4thUivACLcBGAsYHQ/s1280/222.jpg',
+  },
 ];
 
 const Testimonial = () => {
@@ -28,97 +34,23 @@ const Testimonial = () => {
   };
 
   return (
-    <>
-    <p style={{fontSize:"20px"}}>Testimonial</p>
     <div className="testimonial-container">
-      <div className="testimonial-slide">
-        <img src={testimonials[activeIndex].image} alt="Client" className="testimonial-image" />
-        <p className="testimonial-feedback">{testimonials[activeIndex].feedback}</p>
-        <p className="testimonial-name">{testimonials[activeIndex].name}</p>
-      </div>
+      {testimonials.map((testimonial, index) => (
+        <div
+          key={testimonial.id}
+          className={`testimonial-slide ${index === activeIndex ? 'active' : index === activeIndex - 1 || (activeIndex === 0 && index === testimonials.length - 1) ? 'prev' : 'next'}`}
+        >
+          <img src={testimonial.image} alt="Client" className="testimonial-image" />
+          <p className="testimonial-feedback">{testimonial.feedback}</p>
+          <p className="testimonial-name">{testimonial.name}</p>
+        </div>
+      ))}
       <div className="testimonial-buttons">
         <button onClick={prevSlide} className="testimonial-button">Prev</button>
         <button onClick={nextSlide} className="testimonial-button">Next</button>
       </div>
     </div>
-    </>
   );
 };
 
 export default Testimonial;
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import { useState, useEffect } from "react";
-// import "./Testimonial.css";
-// import data from "./data";
-
-
-// const Testimonial = () => {
-//   const [people] = useState(data);
-//   const [index, setIndex] = useState(0);
-
-//   useEffect(() => {
-//     const lastIndex = people.length - 1;
-//     if (index < 0) {
-//       setIndex(lastIndex);
-//     }
-//     if (index > lastIndex) {
-//       setIndex(0);
-//     }
-//   }, [index, people]);
-
-//   useEffect(() => {
-//     let slider = setInterval(() => {
-//       setIndex(index + 1);
-//     }, 3000);
-//     return () => {
-//       clearInterval(slider);
-//     }
-//   }, [index]);
-
-
-//   return (
-//     <>
-//       <p style={{
-//         color: "#444444",
-//         fontWeight: "500",
-//         fontSize: "34px",
-//         paddingTop: "25px"
-//       }}>What Our Client Say About Us</p>
-//       <section classNameName='section '>
-//         <div classNameName='section-center'>
-//           {people.map((item, indexPeople) => {
-//             const { id, name, image, quote } = item;
-//             let position = "nextSlide";
-//             if (indexPeople === index) {
-//               position = "activeSlide"
-//             }
-//             if (indexPeople === index - 1 || (index === 0 && indexPeople === people.length - 1)) {
-//               position = "lastSlide"
-//             }
-//             return (
-//               <article classNameName={position} key={id}>
-//                 <img src={image} alt={name} classNameName="person-img" />
-//                 <h4>{name}</h4>
-//                 <p classNameName='text'>{quote}</p>
-//               </article>
-//             )
-//           })}
-//           <button classNameName='prev' onClick={() => setIndex(index - 1)}><i classNameName='fas fa-arrow-left' /></button>
-//           <button classNameName='next' onClick={() => setIndex(index + 1)}><i classNameName='fas fa-arrow-right' /></button>
-//         </div>
-//       </section>
-//     </>
-
-//   )
-// }
-
-// export default Testimonial// 
