e5c8b4a5dbab7bed6364f7a909ec313794291173
sahill-1 <sv260397@gmail.com>
2023-07-17 10:57:12 +0530

    Registration form updates

--- frontend/src/Components/Registration/RegistrationForm.js
+++ frontend/src/Components/Registration/RegistrationForm.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
 import { useFormik } from "formik";
-import { signupSchema } from './signupSchema';
+import { signupSchema } from "./signupSchema";
 // import BackGround from "../images/download.jpg";
 import "bootstrap";
 
@@ -9,32 +9,72 @@ const initialValues = {
   name: "",
   email: "",
   password: "",
-  confirm_password: ""
-}
+  confirm_password: "",
+  status: "",
+  state: "",
+  city: "",
+};
+
+const states = [
+  "Select a state",
+  "Andhra Pradesh",
+  " Arunachal Pradesh	",
+  "Assam",
+  "Bihar",
+  "Chhattisgarh",
+  "Goa",
+  "Gujarat",
+  "Haryana",
+  "Himachal Pradesh",
+  "Jharkhand",
+  "Karnataka",
+  "Kerala",
+  "Madhya Pradesh",
+  "Maharashtra",
+  "Manipur",
+  "Meghalaya",
+  "Mizoram",
+  "Nagaland",
+  "Odisha",
+  "Punjab",
+  "Rajasthan",
+  "Sikkim",
+  "Tamil Nadu",
+  "Telangana",
+  "Tripura",
+  "Uttar Pradesh	",
+  "Uttarakhand",
+  " West Bengal",
+  "Andaman and Nicobar Islands",
+  "Chandigarh",
+  "Dadra & Nagar Haveli",
+  " Daman & Diu	Daman",
+  "Delhi",
+  "Jammu and Kashmir	",
+  "Lakshadweep",
+  "Puducherry",
+  "Ladakh",
+];
 
 const RegistrationForm = () => {
-
   const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
     useFormik({
       initialValues,
       validationSchema: signupSchema,
       onSubmit: (values, action) => {
-        console.log(
-          values
-        );  
+        console.log(values);
         action.resetForm();
-      }
+      },
     });
   // console.log(
   //   errors
   // );
 
-
   return (
     <>
       <Wrapper>
         {/* Animated CSS BackGround */}
-        <div className="area" >
+        <div className="area">
           <ul className="circles">
             <li></li>
             <li></li>
@@ -47,21 +87,23 @@ const RegistrationForm = () => {
             <li></li>
             <li></li>
           </ul>
-        </div >
-        {/* style={{ backgroundImage: `url(${BackGround})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', opacity: "0.8" }} */}
-        <div className="container" >
-          <div className="modal" >
-            <div className="modal-container">
-              <div className="modal-left">
+        </div>
+        <div className="container">
+          <div className="modal">
+            <div
+              className="modal-container border border-danger"
+              style={{ width: "100%", padding: "2rem 0rem" }}
+            >
+              <div className="modal-left border border-warning">
                 <h1 className="modal-title">Welcome!</h1>
-                <p className="modal-desc">
-                  To <span style={{ fontWeight: "700" }}>Business World Trade</span>
+                <p className="modal-desc text-center">
+                  To{" "}
+                  <span style={{ fontWeight: "700" }}>
+                    Business World Trade
+                  </span>
                 </p>
                 <form onSubmit={handleSubmit}>
                   <div className="input-block">
-                    {/* <label htmlFor="name" className="input-label">
-                      Name
-                    </label> */}
                     <input
                       type="name"
                       autoComplete="off"
@@ -77,9 +119,6 @@ const RegistrationForm = () => {
                     ) : null}
                   </div>
                   <div className="input-block">
-                    {/* <label htmlFor="number" className="input-label">
-                      Phone Number
-                    </label> */}
                     <input
                       type="number"
                       autoComplete="off"
@@ -95,9 +134,6 @@ const RegistrationForm = () => {
                     ) : null}
                   </div>
                   <div className="input-block">
-                    {/* <label htmlFor="email" className="input-label">
-                      Email
-                    </label> */}
                     <input
                       type="email"
                       autoComplete="off"
@@ -113,11 +149,8 @@ const RegistrationForm = () => {
                     ) : null}
                   </div>
                   <div className="input-block">
-                    {/* <label htmlFor="password" className="input-label">
-                      Password
-                    </label> */}
                     <input
-                      type="password"          
+                      type="password"
                       autoComplete="off"
                       name="password"
                       id="password"
@@ -131,34 +164,54 @@ const RegistrationForm = () => {
                     ) : null}
                   </div>
 
-                  {/* FOR COUNTRY */}
                   <div className="input-block">
-                    {/* <label htmlFor="country" className="input-label">
-                      Country
-                    </label> */}
-                    <select name="country" id="country" value="country" onBlur={handleBlur} >
-                      <option value="">Select a country</option>
-                      <option value="">A</option>
-                      <option value="">B</option>
+                    <label htmlFor="status" className="input-label">
+                      Status
+                    </label>
+                    <select
+                      name="status"
+                      id="status"
+                      value={values.status}
+                      onBlur={handleBlur}
+                    >
+                      <option value="">Select...</option>
+                      <option value="">Buyer</option>
+                      <option value="">Seller</option>
+                      <option value="">Other</option>
                     </select>
+                    {errors.status && touched.state ? (
+                      <p className="form-error">{errors.state}</p>
+                    ) : null}
                   </div>
 
-                  <div className="input-block" style={{ display: "flex", flexDirection: "row", justifyContent: "space-around" }} >
+                  <div
+                    className="input-block"
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "space-around",
+                    }}
+                  >
                     {/* FOR STATES */}
                     <div className="input-block">
                       <label htmlFor="states" className="input-label">
                         State
                       </label>
+
                       <select
                         name="states"
-                        id="states"
                         value={values.states}
                         onBlur={handleBlur}
                       >
-                        <option value="">Select a state</option>
-                        <option value="">A</option>
-                        <option value="">B</option>
+                        {states.map((state, index, key) => (
+                          <>
+                            <option name={state} value={state}>
+                              {state}
+                            </option>
+                          </>
+                        ))}
                       </select>
+
                       {errors.state && touched.state ? (
                         <p className="form-error">{errors.state}</p>
                       ) : null}
@@ -197,19 +250,19 @@ const RegistrationForm = () => {
                   Already have an account? <a href="/">Sign In now</a>
                 </p> */}
               </div>
-              <div className="modal-right">
+              {/* <div className="modal-right">
                 <img
                   src="https://images.unsplash.com/photo-1512486130939-2c4f79935e4f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=dfd2ec5a01006fd8c4d7592a381d3776&auto=format&fit=crop&w=1000&q=80"
                   alt=""
                 />
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
       </Wrapper>
     </>
-  )
-}
+  );
+};
 
 const Wrapper = styled.section`
   .container {
@@ -241,7 +294,7 @@ const Wrapper = styled.section`
     max-width: 60vw;
     width: 100%;
     border-radius: 10px;
-    overflow: hidden ;
+    overflow: hidden;
     position: absolute;
     transition-duration: 0.3s;
     background: #fff;
@@ -369,51 +422,81 @@ const Wrapper = styled.section`
       display: none;
     }
   }
-
-
+  @media (max-width: 526px) {
+    .modal-container {
+      max-width: 90vw;
+    }
+    .modal-right {
+      display: none;
+    }
+    .modal-left {
+      padding: 10px 15px;
+    }
+    .modal-title {
+      font-size: 30px;
+    }
+    .input-label {
+      font-size: 10px;
+    }
+    .input-block {
+      padding: 8px 8px 6px;
+    }
+    .input-block input {
+      font-size: 12px;
+    }
+    .input-button {
+      font-size: 12px;
+      padding: 0.8rem 1.6rem;
+    }
+    .modal-buttons a {
+      font-size: 12px;
+    }
+    .form-error {
+      font-size: 0.8rem;
+    }
+  }
 
   //Animated CSS background
-//   @import url('https://fonts.googleapis.com/css?family=Exo:400,700');
+  //   @import url('https://fonts.googleapis.com/css?family=Exo:400,700');
 
-// *{
-//     margin: 0px;
-//     padding: 0px;
-// }
+  // *{
+  //     margin: 0px;
+  //     padding: 0px;
+  // }
 
-// body{
-//     font-family: 'Exo', sans-serif;
-// }
+  // body{
+  //     font-family: 'Exo', sans-serif;
+  // }
 
-
-.context {
+  .context {
     width: 100%;
     position: absolute;
-    top:50vh;
-}
+    top: 50vh;
+  }
 
-.context h1{
+  .context h1 {
     text-align: center;
     color: #fff;
     font-size: 50px;
-}
+  }
 
-.area{
-    background: #4e55c8 !important;  
-    background: -webkit-linear-gradient(to left, #fff, #fff );  
+  .area {
+    background: #4e55c8 !important;
+    background: -webkit-linear-gradient(to left, #fff, #fff);
     width: 100%;
-    height:100vh;
-}
+    height: 100vh;
+  }
 
-.circles{
+  .circles {
     position: absolute;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
     overflow: hidden;
-}
+  }
 
-.circles li{
+  .circles li {
     position: absolute;
     display: block;
     list-style: none;
@@ -423,96 +506,96 @@ const Wrapper = styled.section`
     background: #fff;
     animation: animate 25s linear infinite;
     bottom: -150px;
-}
+  }
 
-.circles li:nth-child(1){
+  .circles li:nth-child(1) {
     left: 15%;
     width: 80px;
     height: 80px;
     animation-delay: 0s;
-}
+  }
 
-.circles li:nth-child(2){
+  .circles li:nth-child(2) {
     left: 10%;
     width: 20px;
     height: 20px;
     animation-delay: 2s;
     animation-duration: 12s;
-}
+  }
 
-.circles li:nth-child(3){
+  .circles li:nth-child(3) {
     left: 70%;
     width: 20px;
     height: 20px;
     animation-delay: 4s;
-}
+  }
 
-.circles li:nth-child(4){
+  .circles li:nth-child(4) {
     left: 10%;
     width: 60px;
     height: 60px;
     animation-delay: 0s;
     animation-duration: 18s;
-}
+  }
 
-.circles li:nth-child(5){
+  .circles li:nth-child(5) {
     left: 25%;
     width: 20px;
     height: 20px;
     animation-delay: 0s;
-}
+  }
 
-.circles li:nth-child(6){
+  .circles li:nth-child(6) {
     left: 76%;
     width: 110px;
     height: 110px;
     animation-delay: 3s;
-}
+  }
 
-.circles li:nth-child(7){
+  .circles li:nth-child(7) {
     left: 35%;
     width: 150px;
     height: 150px;
     animation-delay: 7s;
-}
+  }
 
-.circles li:nth-child(8){
+  .circles li:nth-child(8) {
     left: 50%;
     width: 25px;
     height: 25px;
     animation-delay: 15s;
     animation-duration: 45s;
-}
+  }
 
-.circles li:nth-child(9){
+  .circles li:nth-child(9) {
     left: 20%;
     width: 25px;
     height: 15px;
     animation-delay: 2s;
     animation-duration: 35s;
-}
+  }
 
-.circles li:nth-child(10){
+  .circles li:nth-child(10) {
     left: 90%;
     width: 150px;
     height: 150px;
     animation-delay: 0s;
     animation-duration: 11s;
-}
+  }
 
-@keyframes animate {
-    0%{
-        transform: translateY(0) rotate(0deg);
-        opacity: 1;
-        border-radius: 0;
+  @keyframes animate {
+    0% {
+      transform: translateY(0) rotate(0deg);
+      opacity: 1;
+      border-radius: 0;
     }
 
-    100%{
-        transform: translateY(-1000px) rotate(720deg);
-        opacity: 0;
-        border-radius: 50%;
+    100% {
+      transform: translateY(-1000px) rotate(720deg);
+      opacity: 0;
+      border-radius: 50%;
     }
-}
+  }
 `;
 
 export default RegistrationForm;
