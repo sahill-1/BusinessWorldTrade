336c0af30e5012c2d1336c012f6655666373f909
sahill-1 <sv260397@gmail.com>
2023-07-21 10:27:21 +0530

    Agriculture products at Homepage changes

--- frontend/src/Components/Homepage/AgricultureProduct.jsx
+++ frontend/src/Components/Homepage/AgricultureProduct.jsx
@@ -1,24 +1,48 @@
-import React from 'react'
+import React from "react";
 
 export default function AgricultureProduct(props) {
-    let title = props.name.split(" ");
-    return (
-        <div className='card' style={{ borderRadius: "20px 20px 0 0", width: "80%", margin: "auto", boxShadow: "rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px" }}>
-            <img style={{ width: "100%", height: "250px", borderRadius: "20px 20px 0px 0px" }} className='product--image' src={props.url} alt={props.title} />
-            <h2 style={{ fontSize: "24px" }}>{title.length >= 1 ? title[0] + " " + title[1] + " ..." : title}</h2>
-            <p style={{ color: "gray" }}>Sold by: xyz</p>
-            <p>Add the description about the product.</p>
-            <p>
-                <button style={{
-                    marginBottom: "20px",
-                    padding: "5px 10px",
-                    borderRadius: "10px",
-                    borderColor: "transparent",
-                    boxShadow: "rgb(14,81,118) 0px 10px 10px -10px",
-                    fontSize: "15px",
-                }}>Get Enquiry</button>
-            </p>
-
-        </div>
-    )
+  return (
+    <div
+      className="card"
+      style={{
+        borderRadius: "20px 20px 0px 0px",
+        width: "85%",
+        margin: "auto",
+        boxShadow:
+          "rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px",
+      }}
+    >
+      <img
+        style={{
+          width: "80%",
+          height: "220px",
+          padding: "20px",
+          borderRadius: "20px 20px 0px 0px",
+          margin: "auto",
+        }}
+        className="product--image"
+        src={props.url}
+        alt={props.title}
+      />
+      <h2 style={{ fontSize: "24px", textAlign: "center" }}></h2>
+      <p style={{ color: "gray", textAlign: "center" }}>Sold by: xyz</p>
+      <p style={{ textAlign: "center" }}>
+        Add the description about the product.
+      </p>
+      <p style={{ textAlign: "center" }}>
+        <button
+          style={{
+            marginBottom: "20px",
+            padding: "5px 10px",
+            borderRadius: "10px",
+            borderColor: "transparent",
+            boxShadow: "rgb(14,81,118) 0px 10px 10px -10px",
+            fontSize: "15px",
+          }}
+        >
+          Get Enquiry
+        </button>
+      </p>
+    </div>
+  );
 }
--- frontend/src/Components/Homepage/AgricultureProducts.js
+++ frontend/src/Components/Homepage/AgricultureProducts.js
@@ -1,57 +1,155 @@
-import React from 'react'
+import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "bootstrap";
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import AgricultureProduct from "./AgricultureProduct";
-const AgricultureProducts = () => {
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { Link, useNavigate } from "react-router-dom";
 
+const AgricultureProducts = () => {
+  const Agriculture = [
+    {
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5suxByklKliG7q8T7KVnPdJRQcIcjmnWZEg&usqp=CAU",
+      soldBy: "Pankaj Kapoor",
+      name: "Onion",
+      description: "Wholeseller: Himachal Pradesh",
+    },
+    {
+      img: "https://mynativeshop.com/wp-content/uploads/2020/12/Urad-Dal-White.jpg",
+      soldBy: "Manoj Dosi",
+      name: "Urad Dhulli",
+      description: "Wholeseller: Jaipur, Rajasthan",
+    },
+    {
+      img: "https://5.imimg.com/data5/SELLER/Default/2022/12/PF/XY/RD/181791155/basmati-rice.png",
+      soldBy: "Vishal Tamoli",
+      name: "Basmati Rice",
+      description: "Retailer: Indore, M.P",
+    },
+    {
+      img: "https://m.media-amazon.com/images/I/81FgVq3wBZL._AC_UF1000,1000_QL80_.jpg",
+      soldBy: "Asif Shaikh",
+      name: "Sesame Seed",
+      description: "Wholeseller: Sangola Maharashtra",
+    },
+    {
+      img: "https://i-invdn-com.investing.com/news/Cotton_800x533_L_1601398852.jpg",
+      soldBy: "Ajay Verma",
+      name: "Cotton",
+      description: "Wholeseller: Aligarh, U.P",
+    },
+    {
+      img: "https://s3.amazonaws.com/eit-planttoolbox-prod/media/images/Camellia_sinensis_05_VRYEYryfQp1j.jpg",
+      soldBy: "Mangiram Goyal",
+      name: "Tea",
+      description: "Wholeseller: Agra",
+    },
+    {
+      img: "https://5.imimg.com/data5/SELLER/Default/2021/6/AI/OH/EV/26605812/black-mustard-seeds-big.jpg",
+      soldBy: "Harshit Mishra",
+      name: "Mustard Seeds",
+      description: "Wholeseller: Amethi, U.P",
+    },
+  ];
   const responsive = {
     superLargeDesktop: {
       breakpoint: { max: 4000, min: 1024 },
       items: 4,
-
     },
     desktop: {
       breakpoint: { max: 1024, min: 800 },
-      items: 3
+      items: 3,
     },
     tablet: {
       breakpoint: { max: 800, min: 464 },
-      items: 2
+      items: 2,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
+      items: 1,
+    },
   };
 
-  const [data, setData] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    axios.get("https://fakestoreapi.com/products")
-      .then((res) => {
-        setData(res.data)
-        console.log(res.data)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/api/getcategory"
+        );
+        setCategories(response.data);
+        console.log("Fetched Categories:", response.data);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    };
+
+    fetchData();
   }, []);
 
-  const productList = data.map((item) => <AgricultureProduct key={item.id} name={item.title} url={item.image} desc={item.description} />)
   return (
     <div>
-      <h1 className='my-4' style={{
-        color: "#444444",
-        fontWeight: "700"
-      }}>Agriculture Products</h1>
+      <h1
+        className="my-3"
+        style={{
+          color: "#444444",
+          fontWeight: "700",
+        }}
+      >
+        Agriculture Products
+      </h1>
       <Carousel responsive={responsive}>
-        {productList}
+        {Agriculture.map((product) => (
+          <div
+            key={product.id}
+            className="card"
+            style={{
+              borderRadius: "20px 20px 0px 0px",
+              width: "85%",
+              margin: "auto",
+              boxShadow:
+                "rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px",
+            }}
+          >
+            <img
+              style={{
+                width: "80%",
+                height: "220px",
+                borderRadius: "20px",
+                margin: "auto",
+              }}
+              className="product--image mt-4"
+              src={product.img}
+              alt={product.name}
+            />
+            <h2 style={{ fontSize: "24px", textAlign: "center" }}>
+              {product.name}
+            </h2>
+            <p style={{ color: "gray", textAlign: "center" }}>
+              Sold by: {product.soldBy}{" "}
+            </p>
+            <p style={{ textAlign: "center" }}>{product.description}</p>
+            <p style={{ textAlign: "center" }}>
+              <button
+                style={{
+                  marginBottom: "20px",
+                  padding: "5px 10px",
+                  borderRadius: "10px",
+                  borderColor: "transparent",
+                  boxShadow: "rgb(14,81,118) 0px 10px 10px -10px",
+                  fontSize: "15px",
+                }}
+              >
+                Get Enquiry
+              </button>
+            </p>
+          </div>
+        ))}
       </Carousel>
     </div>
-  )
-}
+  );
+};
 
-export default AgricultureProducts
+export default AgricultureProducts;
