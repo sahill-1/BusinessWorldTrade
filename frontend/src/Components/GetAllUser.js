0c679f7ae74a0c5f90d4e6ebb2144071ee887d95
sahill-1 <sv260397@gmail.com>
2023-08-26 11:51:11 +0530

    get  all users

---
+++ frontend/src/Components/GetAllUser.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+const Sahil = () => {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/api/Getalluser")
+      .then((response) => response.json())
+      .then((res) => {
+        setUser(res);
+        console.log("res", res);
+        console.log("userr", user.name);
+      })
+      .catch((error) => console.log("error", error));
+  }, [setUser]);
+
+  return (
+    <>
+      <Container>
+        <h5>See All Users</h5>
+      </Container>
+      {/* {user.map((el) => {
+        return (
+          <div>
+            <h5>{el.User.name}</h5>
+          </div>
+        );
+      })} */}
+    </>
+  );
+};
+
+export default Sahil;
