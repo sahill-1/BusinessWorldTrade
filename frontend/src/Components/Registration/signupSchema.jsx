31eef8e279df2f4830b1c39a998efddd268f4d51
Sahil Vishwakarma <105960082+sahill-1@users.noreply.github.com>
2026-01-02 20:17:40 +0530

    signupSchema.jsx

--- frontend/src/Components/Registration/signupSchema.jsx
+++ frontend/src/Components/Registration/signupSchema.jsx
@@ -1,11 +1,11 @@
 import * as Yup from "yup";
 
 export const signupSchema = Yup.object({
-  // name: Yup.string().min(2).max(25).required("Please enter your name"),
-  email: Yup.string().email().required("Please enter your email"),
-  // number: Yup.number().required("Please enter valid number").positive().integer(),
-  password: Yup.string().min(6).required("Please enter your password"),
-  // confirm_password: Yup.string()
-  //   .required()
-  //   .oneOf([Yup.ref("password"), null], "Password must match"),
+  name: Yup.string().min(2).max(25).required("Please enter your name"),
+  email: Yup.string().email().required("Please eneter your email"),
+  number: Yup.number().required("Please enter valid number").positive().integer(),
+  password: Yup.string().min(6).required("Please enter ypour password"),
+  confirm_password: Yup.string()
+     .required()
+     .oneOf([Yup.ref("password"), null], "Password must match"),
 });
